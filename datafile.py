78dbd5ca729fd2cb995f4935eb8ee3193220354f
peter-hogg <peterhogg2006@gmail.com>
2023-10-18 21:04:33 -0700

    Adding files

---
+++ datafile.py
@@ -0,0 +1,107 @@
+import os
+import numpy as np
+import mmap
+import scipy.io
+import h5py
+
+from .subclasses.metadata import MetaData
+from .utils.file_header import load_file_header_v2
+
+class DataFile():
+
+    def __init__(self, datfile):
+        self.MAGIC_NUMBER = np.uint32(322379495)
+
+        self.filename = datfile
+        self.metaDataFileName = ''
+        self.datFileName = ''
+        self.rawData = []
+        self.metaData = None
+        self.StreamId = None
+        self.header = None
+        self.lineHeaderIdxs = None
+        self._load_file()
+        
+        
+        """#Attributes to fill in
+        numCycles
+        lineHeaderIdxs;
+        lineDataStartIdxs;
+        lineDataNumElements;
+        fastZs
+        lineSuperPixelIDs
+        lineSuperPixelZIdxs
+        lineNumSuperPixels
+        zPixelReplacementMaps
+        zPixelReplacementMapsNonRedundant
+        lineFastZIdxs
+        totalNumLines
+        numChannels
+        firstLineTimestamp (1,1) uint64 = 0;
+        """
+        
+        
+        
+    def _load_file(self):
+        base_dir, filename = os.path.split(self.filename)
+        n_base = os.path.splitext(filename)[0].replace('-TRIAL', '', -1).strip()
+
+        self.metaDataFileName = os.path.join(base_dir, n_base + '.meta')
+        print(self.metaDataFileName)
+        self.datFileName = os.path.join(base_dir, filename)
+
+        if not os.path.isfile(self.metaDataFileName):
+            raise FileNotFoundError('Metadata file not found.')
+        self.metaData = MetaData(self.metaDataFileName)
+        print('MetaData Loaded')
+        
+        
+        if not os.path.isfile(self.datFileName):
+            raise FileNotFoundError('Data file not found.')
+        self.rawData = np.memmap(self.filename, dtype='uint32')
+        self.header = self.load_file_header(self.rawData)
+            
+    # Function for loading file header:
+    def load_file_header(self, rawData):
+        raw_data = np.frombuffer(rawData, dtype=np.uint32)
+        if raw_data.dtype != 'uint32':
+            raw_data.dtype('uint32')
+
+        file_magic_number = raw_data[0]
+        assert file_magic_number == self.MAGIC_NUMBER, 'Data format error. This is not a SLAP2 data file.'
+        
+        
+        # Currently have only implemented file version 2
+        file_format_version = raw_data[1]
+        assert file_format_version <= 2, 'Unknown format version'
+        if file_format_version == 2:
+            header = load_file_header_v2(self, raw_data)
+        else:
+            raise ValueError(f'Unknown file format version: {file_format_version}')
+
+        # Load Indices
+        raw_data  =  np.frombuffer(raw_data, dtype=np.uint16)
+        if raw_data.dtype != 'uint16':
+            raw_data.astype('uint16')
+
+        line_idxs = np.zeros(int(header['linesPerCycle']), dtype=int)
+        line_size_bytes = np.zeros(int(header['linesPerCycle']), dtype=np.uint32)
+        line_idxs[0] = header['firstCycleOffsetBytes'] // 2 + 1
+
+        line_size_bytes[0] = np.frombuffer(rawData[line_idxs[0]:line_idxs[0] + 2], dtype=np.uint32)[0]
+
+        for idx in range(1, int(header['linesPerCycle'])):
+            line_idxs[idx] = line_idxs[idx - 1] + line_size_bytes[idx - 1] // 2
+            line_size_bytes[idx] = np.frombuffer(rawData[line_idxs[idx]:line_idxs[idx] + 2], dtype=np.uint32)[0]
+
+        line_header_idxs = line_idxs
+        line_data_start_idxs = line_idxs + header['lineHeaderSizeBytes'] // 2
+        line_data_num_elements = (line_size_bytes - header['lineHeaderSizeBytes']) // 2
+
+
+        # May need to update this conditional in the future
+        if not 'referenceTimestamp' in list(header.keys()):
+            header.referenceTimestamp = np.uint64(0)
+            first_line_header = obj.get_line_header(1, 1)
+            obj.header.referenceTimestamp = first_line_header.timestamp
+        return header
---
+++ utils/file_header.py
@@ -0,0 +1,101 @@
+# Utility functions for the Slap2DataFile
+def load_file_header_v2(obj, rawUint32):
+    # Data format:
+    # uint32_t magic_start          = MAGIC_NUMBER;
+    # uint32_t file_version         = 1;
+    # uint32_t fileHeaderSize_Bytes = XXXX;
+    #
+    # uint32_t fieldId  = XXXX;
+    # uint32_t fieldVal = XXXX;
+    # uint32_t fieldId  = XXXX;
+    # uint32_t fieldVal = XXXX;
+    #
+    # ...
+    #
+    # uint32_t magic_end           = MAGIC_NUMBER;
+
+    
+    file_magic_number = rawUint32[0]
+    assert file_magic_number == obj.MAGIC_NUMBER, 'Data format error.'
+
+    file_format_version = rawUint32[1]
+    assert file_format_version == 2, 'Unknown format version'
+
+    file_header_size_bytes = rawUint32[2]
+    file_header_entries = file_header_size_bytes // 4
+
+    
+    header_end_magic_number = rawUint32[int(file_header_entries)-1]
+    assert header_end_magic_number == obj.MAGIC_NUMBER, 'Data corruption in file header.'
+
+    field_value_pairs = rawUint32[3:file_header_entries - 1]
+    field_value_pairs = field_value_pairs.reshape(-1, 2)
+
+    header_ = translate_field_value_pairs(field_value_pairs)
+    header_ = translate_channel_mask(header_)
+    header_ = translate_reference_timestamp(header_)
+    header_['file_version'] = file_format_version
+    header_['magic_start'] = file_magic_number
+    header_['magic_end'] = header_end_magic_number
+    header = header_
+
+    file_size_bytes = len(rawUint32) * 4
+    num_cycles = int((file_size_bytes - header_['firstCycleOffsetBytes']) / header_['bytesPerCycle'])
+    total_num_lines = num_cycles * header['linesPerCycle']
+    num_channels = float(header['numChannels'])
+    return header
+
+def translate_field_value_pairs(field_value_pairs):
+    file_header_fields = [
+        "firstCycleOffsetBytes",
+        "lineHeaderSizeBytes",
+        "laserPathIdx",
+        "bytesPerCycle",
+        "linesPerCycle",
+        "superPixelsPerCycle",
+        "dmdPixelsPerRow",
+        "dmdPixelsPerColumn",
+        "numChannels",
+        "channelMask",
+        "numSlices",
+        "channelsInterleave",
+        "fpgaSystemClock_Hz",
+        "referenceTimestamp_lower",
+        "referenceTimestamp_upper"
+    ]
+
+    map = {i: field for i, field in enumerate(file_header_fields)}
+
+    struct_out = {}
+    for idx in range(field_value_pairs.shape[0]):
+        field = field_value_pairs[idx, 0]
+        value = field_value_pairs[idx, 1]
+
+        if field in map:
+            field = map[field]
+            struct_out[field] = float(value)
+        else:
+            print(f'Warning: Unknown field/value pair in header: fieldID={field} value={value}')
+
+    return struct_out
+
+def translate_channel_mask(header):
+    assert 'channelMask' in header
+    channels = [bit for bit in range(32) if (int(header['channelMask']) & (1 << bit)) != 0]
+    header['channels'] = channels
+
+    assert 'numChannels' in header
+    assert len(channels) == header['numChannels'], 'Data integrity error: header field ''numChannels'' does not agree with header field ''channelMask'''
+    return header
+
+def translate_reference_timestamp(header):
+    print(header)
+    print(header['referenceTimestamp_lower'])
+    print(header['referenceTimestamp_upper'])
+
+    
+    if 'referenceTimestamp_lower' in header and 'referenceTimestamp_upper' in header:
+        reference_timestamp_lower = header['referenceTimestamp_lower']
+        reference_timestamp_upper = int(header['referenceTimestamp_upper']) << 32
+        header['referenceTimestamp'] = int(reference_timestamp_lower) | reference_timestamp_upper
+    return header
