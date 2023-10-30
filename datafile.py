6591d68fcd465865e13e31f084c10d256eda18f5
Jerry Tong <65472254+JerryTong-GH@users.noreply.github.com>
2023-10-30 15:51:12 -0700

    Update datafile.py

--- datafile.py
+++ datafile.py
@@ -4,11 +4,10 @@ import mmap
 import scipy.io
 import h5py
 
-#from .subclasses.metadata import MetaData
-#from .utils.file_header import load_file_header_v2 -> This is relative path and it works on how you set up but I just rewrite it
+from subclasses.metadata import MetaData
+from utils.file_header import load_file_header_v2 
 
-import subclasses.metadata
-import utils.file_header
+
 
 
 class DataFile():
