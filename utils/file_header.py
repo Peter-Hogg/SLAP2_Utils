a64fed11d01ec49b6f810eddca5a47393745422d
Jerry Tong <65472254+JerryTong-GH@users.noreply.github.com>
2023-10-30 18:16:06 -0700

    Update file_header.py

--- utils/file_header.py
+++ utils/file_header.py
@@ -90,8 +90,7 @@ def translate_channel_mask(header):
 
 def translate_reference_timestamp(header):
     print(header)
-    print(header['referenceTimestamp_lower'])
-    print(header['referenceTimestamp_upper'])
+
 
     
     if 'referenceTimestamp_lower' in header and 'referenceTimestamp_upper' in header:
