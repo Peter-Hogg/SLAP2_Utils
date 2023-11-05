a3c92464b1db76007d3b64313c1794a36876b464
Peter Hogg <peterhogg2006@gmail.com>
2023-11-04 18:45:54 -0700

    Updates

--- main.py
+++ main.py
@@ -5,36 +5,44 @@ from SLAP2_Utils.datafile import DataFile
 from SLAP2_Utils.subclasses.metadata import MetaData
 from SLAP2_Utils.utils.trace import Trace
 
-from skimage.draw import polygon_perimeter
+
 
 hDataFile = DataFile('TestData/Tadpole2_20230829_183310_DMD2.dat');
-traces = {}
-for _roi in range(len(hDataFile.metaData.AcquisitionContainer.ROIs)):
-    print(_roi)
-    roi_shape = hDataFile.metaData.AcquisitionContainer.ROIs[_roi].shapeData
-    zIdx = 1;
-    chIdx = 1;
-    hTrace = Trace(hDataFile,zIdx,chIdx);
+roi_shape = hDataFile.metaData.AcquisitionContainer.ROIs[0].shapeData
+
+#metaData.AcquisitionContainer.ROIs[0].shapeData
+zIdx = 1
+chIdx = 1
+hTrace = Trace(hDataFile,zIdx,chIdx);
 
 
-    #
-    roi_shape= roi_shape.astype(int)
-    img = np.zeros((800, 1280), dtype=np.uint8)
-    for _xy in zip(roi_shape[0,:], roi_shape[1,:]):
-        img[_xy[0]-1, _xy[1]-1]=1
+from skimage.draw import polygon_perimeter
 
-    #plt.imshow(img)
-    #plt.show()
-    rasterPixels = np.full((800, 1280), False)
-    integrationPixels = img.astype(np.bool_)
+roi_shape = hDataFile.metaData.AcquisitionContainer.ROIs[0].shapeData
 
-    hTrace.setPixelIdxs(rasterPixels, integrationPixels);
-    __trace, _, _, _ = hTrace.process(10, 100)
-    plt.plot(__trace[:1000])
-    plt.show()
-    traces[_roi] = __trace
-    #traces[_roi]=_trace
+img = np.zeros((800, 1280), dtype=np.uint8)
 
-print(traces)
+rr, cc = polygon_perimeter(roi_shape[0,:], roi_shape[1,:],
+
+                           shape=img.shape, clip=True)
+
+img[rr, cc] = 1
+
+plt.imshow(img)
+
+#rasterPixels = pixelMask;
+#integrationPixels = pixelMask;
+
+pixelMask=np.full((800, 1280), True)
+rasterPixels = pixelMask;
+integrationPixels = pixelMask;
+
+hTrace.setPixelIdxs(rasterPixels, integrationPixels);
+#windowWidth_lines = 10;
+#expectedWindowWidth_lines = 100;
+#trace = hTrace.process(windowWidth_lines,expectedWindowWidth_lines);
+_trace = hTrace.process(10, 100)
+plt.plot(_trace)
+
 
 
