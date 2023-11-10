7e545713054f23f8c9765095dba006c1b25bc3a4
Peter Hogg <peterhogg2006@gmail.com>
2023-11-10 13:10:06 -0800

    Added function for stim times

---
+++ slap2_utils/functions/stim.py
@@ -0,0 +1,15 @@
+import h5py
+import numpy as np
+from scipy import signal
+
+
+def returnStimTime(stimFile, distance=250, hz=5000):
+    # stimlog is a hf file containing TTL traces from the SLAP2 datarecorder
+    # TTL trace is sampled at 5000 hz
+    # Returns stop and start of visual stim in seconds
+    stimLog = h5py.File(stimFile, 'r')
+    TTL_Trace = stimLog['TTL'][:]
+    peaks, _ = signal.find_peaks(np.diff(TTL_Trace), height=2.5, distance=distance)
+    start = [_peaks[i]*(1/hz) for i in range(0, len(_peaks), 2)]
+    stop = [_peaks[i]*(1/hz) for i in range(1, len(_peaks), 2)]
+    return start, stop
