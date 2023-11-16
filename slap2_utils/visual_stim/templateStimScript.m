140fe78e4af36b184864ee2297654d9756c99f3e
peter-hogg <peterhogg2006@gmail.com>
2023-11-15 18:06:09 -0800

    Stim Script Triggered by SLAP2

--- slap2_utils/visual_stim/templateStimScript.m
+++ slap2_utils/visual_stim/templateStimScript.m
@@ -1,48 +1,59 @@
-function listenForRisingEdge(daqDevice, channel, threshold, callback)
+
+% Example usage
+[myWin, winRect] = CreateStimWindow();
+
+daqDevice = 'Dev1'; % Replace with your DAQ device name or ID
+channel = 'ai1';   % Replace with the channel you want to listen to
+threshold =4.8;   % Adjust the threshold as needed
+callback = @myCallbackFunction; % Replace with your custom callback function
+listenForRisingEdge(daqDevice, channel, threshold, callback, myWin);
+
+
+
+
+
+function listenForRisingEdge(daqDevice, channel, threshold, callback, window)
+    persistent DAQses,
     % Listen for a rising edge on the specified channel of the DAQ device.
     % When a rising edge is detected, execute the provided callback function.
-
+    DAQ6001 = daq('ni')
+    stimTrig = addoutput(DAQ6001, "Dev1", "ao0", "Voltage");
     % Create a DAQ session
-    s = daq.createSession('ni');
-    
+    DAQses = daq.createSession('ni');
+
+
     % Add an analog input channel (or digital input channel) for listening
-    addAnalogInputChannel(s, daqDevice, channel, 'Voltage');
-    
+    ai1 = addAnalogInputChannel(DAQses, daqDevice, channel, 'Voltage');
     % Configure the input range and trigger settings
-    s.Rate = 1000; % Set the sample rate (adjust as needed)
-    s.IsContinuous = true;
-    s.TriggersPerRun = inf;
-    
+    DAQses.Rate = 1000; % Set the sample rate (adjust as needed)
+    DAQses.IsContinuous = true;
+    DAQses.TriggersPerRun = 1;    
     % Define the trigger condition (rising edge detection)
-    listener = s.addlistener('DataAvailable', @(src, event) detectRisingEdge(event, threshold, callback));
+    lh = DAQses.addlistener('DataAvailable', @(src, event) detectRisingEdge(event, threshold, callback));
     
     % Start the DAQ session
-    startBackground(s);
-end
+    startBackground(DAQses);
 
-function detectRisingEdge(event, threshold, callback)
-    % This function is called when data is available from the DAQ device.
-    % It checks for a rising edge and executes the callback if detected.
+
+    function detectRisingEdge(event, threshold, callback)
+        data = event.Data
+        % Check for a rising edge (assuming the input is a voltage signal)
+        if any(event.Data > threshold) %&& data(end-1) <= threshold
+            % Rising edge detected, execute the callback
+            myCallbackFunction();
+           
+        end
+    end
     
-    data = event.Data;
     
-    % Check for a rising edge (assuming the input is a voltage signal)
-    if data(end) > threshold && data(end-1) <= threshold
-        % Rising edge detected, execute the callback
-        callback();
+    % Define your custom callback function
+    function myCallbackFunction()
+        disp('Rising edge detected!'); % Replace with your desired action
+        delete(lh)
+    
+        %[t, sType, angle, speed] = 
+        pseudoRandGrating(window, DAQ6001)
     end
 end
 
 
-% Example usage
-daqDevice = 'Dev1'; % Replace with your DAQ device name or ID
-channel = 'ai0';   % Replace with the channel you want to listen to
-threshold = 2.0;   % Adjust the threshold as needed
-callback = @myCallbackFunction; % Replace with your custom callback function
-
-listenForRisingEdge(daqDevice, channel, threshold, callback);
-
-% Define your custom callback function
-function myCallbackFunction()
-    disp('Rising edge detected!'); % Replace with your desired action
-end
