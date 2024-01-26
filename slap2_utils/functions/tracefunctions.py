a3438827933accf2dda1158e103823049dc9b0ac
Peter Hogg <peterhogg2006@gmail.com>
2024-01-26 13:14:51 -0800

    updates

--- slap2_utils/functions/tracefunctions.py
+++ slap2_utils/functions/tracefunctions.py
@@ -1,21 +1,19 @@
-from slap2.utils import trace
+import numpy as np
+from skimage.draw import polygon_perimeter
+ 
+from ..utils import trace
 
-def returnAllTrace(datafile, roiIndex, chIdx=1,  zIdx=1, window=10, expectedWindowWidth=100):
+def returnAllTrace(datafile, chIdx=1,  zIdx=1, window=10, expectedWindowWidth=100):
     # Returns dictionary of traces. key is ROI index 
     traces  = {}
-   
-    hTrace = Trace(exp1,zIdx,chIdx)
+    hTrace = trace.Trace(datafile,zIdx,chIdx)
 
    
-    for _roi in range(len(exp1.metaData.AcquisitionContainer.ROIs)):
-        
-        roi_shape = exp1.metaData.AcquisitionContainer.ROIs[_roi].shapeData
-
+    for _roi in range(len(datafile.metaData.AcquisitionContainer.ROIs)):
+        roi_shape = datafile.metaData.AcquisitionContainer.ROIs[_roi].shapeData
         img = np.zeros((800, 1280), dtype=np.uint8)
-
         rr, cc = polygon_perimeter(roi_shape[0,:],
                                 roi_shape[1,:],
-
                                 shape=img.shape, clip=True)
 
         img[rr, cc] = 1
@@ -30,12 +28,12 @@ def returnAllTrace(datafile, roiIndex, chIdx=1,  zIdx=1, window=10, expectedWind
     return traces
 
 def cleanVolumeTrace():
-    
+    return None
 
 
 def returnVolumeTrace(datafile, roiIndex, chIdx=1):
     ROI = datafile.metaData.AcquisitionContainer.ROIs[roiIndex]
-    
+    zIdx = datafile.fastz.index(ROI.z)
     roi_shape = ROI.shapeData
     img = np.zeros((800, 1280), dtype=np.uint8)
     rr, cc = polygon_perimeter(roi_shape[0,:],
@@ -67,6 +65,18 @@ def returnVolumeTrace(datafile, roiIndex, chIdx=1):
         mask[img==1]=True
         rasterPixels = np.full((800, 1280), False, dtype=bool)
         integrationPixels = mask
+
+        dataIndex = []
+        for _idx _sliceIdx in enumerate(acqParsePlan['sliceIdx']):
+            if zId in _sliceIDx:
+                dataIndex.append(_idx)
+
+        trace_group=[]
+        for traceIDx in range(0, _trace.shape[0], len(acqParsePlan['sliceIdx']):
+            for _idx in dataIndex:
+                trace_group.append([_trace[traceIDx + _idx]])
+
+
 
         hTrace.setPixelIdxs(rasterPixels,integrationPixels)
         # windowWidth_lines and  expectedWindowWidth_lines set to 1
