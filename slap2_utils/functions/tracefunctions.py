cd8483ba147b45a5bc2756c3f8225fb1119b918a
peter-hogg <peterhogg2006@gmail.com>
2024-01-26 14:47:16 -0800

    volumeTrace added

--- slap2_utils/functions/tracefunctions.py
+++ slap2_utils/functions/tracefunctions.py
@@ -27,13 +27,24 @@ def returnAllTrace(datafile, chIdx=1,  zIdx=1, window=10, expectedWindowWidth=10
         traces[_roi] = _trace
     return traces
 
-def cleanVolumeTrace():
-    return None
+def cleanVolumeTrace(datafile, zId, trace):
+    sliceIdx = datafile.metaData.AcquisitionContainer.ParsePlan['acqParsePlan']['sliceIdx']
+    zId =0
+    dataIndex = []
+    for _idx, _sliceIdx in enumerate(sliceIdx):
+        if _sliceIdx.shape[0] ==1:
+            if zId in _sliceIdx:
+                dataIndex.append(_idx)
+    cleanTrace = []
+    for x in range(0, trace.shape[0], len(sliceIdx)):
+        cleanTrace.append(np.median(trace[np.array(dataIndex)+x]))
+    return np.array(cleanTrace)
 
 
 def returnVolumeTrace(datafile, roiIndex, chIdx=1):
     ROI = datafile.metaData.AcquisitionContainer.ROIs[roiIndex]
-    zIdx = datafile.fastz.index(ROI.z)
+    zIdx = datafile.fastZs.index(ROI.z)
+    hTrace = trace.Trace(datafile,zIdx,chIdx)
     roi_shape = ROI.shapeData
     img = np.zeros((800, 1280), dtype=np.uint8)
     rr, cc = polygon_perimeter(roi_shape[0,:],
@@ -42,44 +53,14 @@ def returnVolumeTrace(datafile, roiIndex, chIdx=1):
                             shape=img.shape, clip=True)
 
     img[rr, cc] = 1
+    mask = np.full((800, 1280), False, dtype=bool)
+    mask[img==1]=True
+    rasterPixels = np.full((800, 1280), False, dtype=bool)
+    integrationPixels = mask
+    hTrace = trace.Trace(datafile,zIdx,chIdx)
 
-
-    traces  = {}
-    hTrace = Trace(exp1,zIdx,chIdx)
-
+    hTrace.setPixelIdxs(rasterPixels,integrationPixels)
+    _trace, _, _, _ = hTrace.process(1, 1)
+    _trace = cleanVolumeTrace(datafile, zIdx, _trace)
+    return _trace
    
-    for _roi in range(len(exp1.metaData.AcquisitionContainer.ROIs)):
-        
-        roi_shape = exp1.metaData.AcquisitionContainer.ROIs[_roi].shapeData;
-
-        img = np.zeros((800, 1280), dtype=np.uint8)
-
-        rr, cc = polygon_perimeter(roi_shape[0,:],
-                                roi_shape[1,:],
-
-                                shape=img.shape, clip=True)
-
-        img[rr, cc] = 1
-        mask = np.full((800, 1280), False, dtype=bool)
-        #mask[img==1] = True
-        mask[img==1]=True
-        rasterPixels = np.full((800, 1280), False, dtype=bool)
-        integrationPixels = mask
-
-        dataIndex = []
-        for _idx _sliceIdx in enumerate(acqParsePlan['sliceIdx']):
-            if zId in _sliceIDx:
-                dataIndex.append(_idx)
-
-        trace_group=[]
-        for traceIDx in range(0, _trace.shape[0], len(acqParsePlan['sliceIdx']):
-            for _idx in dataIndex:
-                trace_group.append([_trace[traceIDx + _idx]])
-
-
-
-        hTrace.setPixelIdxs(rasterPixels,integrationPixels)
-        # windowWidth_lines and  expectedWindowWidth_lines set to 1
-        _trace, _, _, _ = hTrace.process(1,1)
-        traces[_roi] = _trace
-        
