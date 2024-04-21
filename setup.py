ffe26c348d0926fe7523e9ba95997c9b45f96ce9
peter-hogg <peterhogg2006@gmail.com>
2024-04-21 11:19:38 -0700

    setup tools

---
+++ setup.py
@@ -0,0 +1,53 @@
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open('requirements.txt', 'r') as f:
+    required = f.read().splitlines()
+
+
+
+setup(
+
+    name='SLAP2_Utils', 
+    version='0.0.3',  
+
+    
+    description='Code to support using a SLAP2 Microscope',  
+
+    url='https://github.com/Peter-Hogg/SLAP2_Utils',  
+
+    
+    author='Peter Hogg',  
+
+    
+    author_email='peter.hogg@ubc.ca',  
+    
+    classifiers=[ 
+        
+        'Development Status :: 4 - Beta',
+
+
+        'Intended Audience :: Researchers',
+
+   
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+
+
+    keywords='python two photon microscope', 
+
+    
+    packages=find_packages(exclude=['*.test']), 
+
+
+    install_requires=required,  
+
+
+)
