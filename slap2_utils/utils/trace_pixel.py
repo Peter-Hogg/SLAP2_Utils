64a2b1f52e24f180f412d8700ffaef7b93a94651
Jerry Tong <65472254+JerryTong-GH@users.noreply.github.com>
2024-05-21 17:56:19 -0700

    Cleaned up trace_pixel.py

--- slap2_utils/utils/trace_pixel.py
+++ slap2_utils/utils/trace_pixel.py
@@ -4,108 +4,69 @@ import trace
 
 class TracePixel:
     def __init__(self):
-        self.fileName = ""
-        self.superPixelId = 0
-        self.superPixelNumPixels = 0
-        self.superPixelNumPixelsSelected = 0
-        self.byteOffsets = []
-        self.lineIdxs = []
-        self.data = None
-        self.isLoaded = False
-        self.firstCycleOffsetBytes = 0
-        self.numCycles = 0
-        self.bytesPerCycle = 0
-        self.linesPerCycle = 0
-        self.y = -1
+        self.fileName = ""  # Name of the file to be loaded
+        self.superPixelId = 0  # Identifier for the superpixel
+        self.superPixelNumPixels = 0  # Number of pixels in the superpixel
+        self.superPixelNumPixelsSelected = 0  # Number of selected pixels in the superpixel
+        self.byteOffsets = []  # List of byte offsets for data extraction
+        self.lineIdxs = []  # List of line indices for data extraction
+        self.data = None  # Loaded data
+        self.isLoaded = False  # Flag indicating whether data is loaded
+        self.firstCycleOffsetBytes = 0  # Offset for the first cycle in bytes
+        self.numCycles = 0  # Number of cycles
+        self.bytesPerCycle = 0  # Number of bytes per cycle
+        self.linesPerCycle = 0  # Number of lines per cycle
+        self.y = -1  # Unspecified variable, initialized to -1
 
     def load(self, hMemmap=None):
+        # Check if data is already loaded
         allLoaded = self.isLoaded
         if allLoaded:
             return
 
-        loadedFilename = ""
+        loadedFilename = ""  # Initialize loaded filename
 
         if hMemmap is not None:
+            # If a memory map object is provided, get its filename and set the format
             loadedFilename = hMemmap.Filename
             hMemmap.Format = 'int16'
 
+        # Update filename to change the extension from .meta to .dat
         fileName_ = self.fileName
         fileName_ = re.sub(r'\.meta$', '.dat', fileName_, flags=re.IGNORECASE)
 
         if loadedFilename != fileName_:
+            # Create a new memory map if the filename does not match
             hMemmap = np.memmap(fileName_, dtype='int16', mode='r')
             loadedFilename = fileName_
+
+        # Calculate byte offsets for each cycle
         cycleIdxs = np.arange(0, self.numCycles, dtype=np.uint64)
         cycleByteOffsets = self.firstCycleOffsetBytes + cycleIdxs * self.bytesPerCycle
 
-        cycleSampleOffsets = [int (x // 2) for x in cycleByteOffsets] 
-        sampleOffsets = [int (x // 2) for x in self.byteOffsets] 
-        
+        # Convert byte offsets to sample offsets (assuming 2 bytes per sample)
+        cycleSampleOffsets = [int(x // 2) for x in cycleByteOffsets]
+        sampleOffsets = [int(x // 2) for x in self.byteOffsets]
+
+        # Create a 2D array of sample offsets
         _sampleOffsets = np.zeros((len(self.byteOffsets), len(cycleSampleOffsets)), dtype='uint64')
-        #sampleOffsets = np.uint64(np.array(sampleOffsets).flatten()) + cycleSampleOffsets
-        
         for i in range(len(self.byteOffsets)):
-            
-            _sampleOffsets[i,:] = sampleOffsets[i] + np.array(cycleSampleOffsets)
+            _sampleOffsets[i, :] = sampleOffsets[i] + np.array(cycleSampleOffsets)
         
         sampleOffsets = _sampleOffsets
+        sampleOffsets = sampleOffsets.astype('int64')  # Ensure offsets are in int64 format
 
-        sampleOffsets = sampleOffsets.astype('int64')
- 
+        # Ensure the offsets do not exceed the length of the memory-mapped file
+        if (sampleOffsets[0, :][len(sampleOffsets[0, :]) - 1]) > len(hMemmap):
+            sampleOffsets[0, :][len(sampleOffsets[0, :]) - 1] = len(hMemmap)
 
-        if (sampleOffsets[0,:][len(sampleOffsets[0,:])-1])>len(hMemmap):
-            sampleOffsets[0,:][len(sampleOffsets[0,:])-1]=len(hMemmap)
- 
-        data_ = []
+        data_ = []  # List to store the loaded data
+        # Extract data from the memory-mapped file using the calculated offsets
         for i in range(sampleOffsets.shape[0]):
-            
-            data_.append(np.take(hMemmap,(sampleOffsets[i,:]-1),0))
-        self.data = data_
+            data_.append(np.take(hMemmap, (sampleOffsets[i, :] - 1), 0))
+        
+        self.data = data_  # Assign the loaded data to the class attribute
+        self.isLoaded = True  # Mark data as loaded
 
-        self.isLoaded = True
 
-    # Never used, architecture only allows this to be set up in Trace
-    def process(self, windowWidth_lines, expectedWindowWidth_lines):
-        self.load()
-
-        tempKernel = np.exp(-np.abs(np.arange(-4*windowWidth_lines, 4*windowWidth_lines+1) / windowWidth_lines))
-        tempKernel = tempKernel.astype('float32')
-
-        expectedKernel = np.ones(expectedWindowWidth_lines, dtype='float32')
-
-        if self.linesPerCycle == 0 or self.numCycles == 0:
-            numLines = 0
-        else:
-            numLines = self.linesPerCycle * self.numCycles
-
-        sumDataWeighted = np.zeros(numLines, dtype='float32')
-        sumExpected = np.zeros(numLines, dtype='float32')
-        sumExpectedWeighted = np.zeros(numLines, dtype='float32')
-
-        lineData = np.zeros((self.linesPerCycle, self.numCycles), dtype='float32')
-        sampled = np.zeros((self.linesPerCycle, self.numCycles), dtype='float32')
-
-        lineData[self.lineIdxs, :] = self.data
-        sampled[self.lineIdxs, :] = 1
-
-        lineData = lineData.flatten()
-        sampled = sampled.flatten()
-
-        spatialWeight = self.superPixelNumPixelsSelected / self.superPixelNumPixels
-        weightedData = np.convolve(lineData, tempKernel * spatialWeight, mode='same')
-        tempWeights = np.convolve(sampled, tempKernel, mode='same')
-
-        expected = np.convolve(lineData, expectedKernel, mode='same')
-        expectedN = np.convolve(sampled, expectedKernel, mode='same')
-        expected = expected / (expectedN + np.finfo(float).eps)
-
-        expectedWeighted = expected * tempWeights
-
-        sumDataWeighted += weightedData
-        sumExpected += expected
-        sumExpectedWeighted += expectedWeighted
-
-        trace = sumDataWeighted / (sumExpectedWeighted + np.finfo(float).eps) * sumExpected
-
-        return trace, sumDataWeighted, sumExpected, sumExpectedWeighted
 
