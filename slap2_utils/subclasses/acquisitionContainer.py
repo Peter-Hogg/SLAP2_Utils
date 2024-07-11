ba23788c13a1821d48f83a9fcd45accbdeac7f1f
Jerry Tong <65472254+JerryTong-GH@users.noreply.github.com>
2024-07-11 13:59:50 -0700

    Update acquisitionContainer.py

--- slap2_utils/subclasses/acquisitionContainer.py
+++ slap2_utils/subclasses/acquisitionContainer.py
@@ -2,6 +2,38 @@ import h5py
 from .slapROI import slapROI
 
 class AcquisitionContainer():
+    """
+        A class used to load a SLAP2 binary metadatafile for data processing. This is also an attribute for the datafile object
+
+        Attributes
+        ----------
+        AcquisitionPlan : dict
+            A dictionary that contains 3 subinstances of ['AcquisitionContainer'], ['ParsePlan'], and ['acqParsePlan'].
+        DmdPatternSequence : list
+            A list containing information related to the patterning of the sequences.
+        ParsePlan : dict
+            A dictionary that contains various instances related to how region of interest is parsed through.
+        ROIs : list
+            A list that contains information related to the region of interests.
+        ScannerParameters : dict
+            A dictionary that stores information related to the scanned parameter (not implemented yet, consider removing).
+        constructor : str
+            A string that describes how the plan is constructed.
+            
+            
+        Descriptions for methods:
+        ----------
+
+        Methods
+        ----------
+        __init__() :
+            Fill the fields mentioned above.
+
+        Return
+        -------
+            Self with populated fields. 
+                
+    """
     
     def __init__(self, metadatafile):
         self.AcquisitionPlan = None
