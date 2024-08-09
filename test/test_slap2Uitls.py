a9c7780247dce10806c278b20f200b745636c8c0
peter-hogg <peterhogg2006@gmail.com>
2024-08-09 14:10:30 -0700

    Test fixes

--- test/test_slap2Uitls.py
+++ test/test_slap2Uitls.py
@@ -2,26 +2,26 @@ from slap2_utils.datafile import DataFile
 
 def test_datafile():
 
-    refHeader =     {'firstCycleOffsetBytes': 136.0,
-                    'lineHeaderSizeBytes': 32.0,
-                    'laserPathIdx': 0.0,
-                    'bytesPerCycle': 398.0,
-                    'linesPerCycle': 6.0,
-                    'superPixelsPerCycle': 103.0,
-                    'dmdPixelsPerRow': 1280.0,
-                    'dmdPixelsPerColumn': 800.0,
-                    'numChannels': 1.0,
-                    'channelMask': 2.0,
-                    'numSlices': 1.0,
-                    'channelsInterleave': 0.0,
-                    'fpgaSystemClock_Hz': 200000000.0,
-                    'referenceTimestamp_lower': 649668779.0,
-                    'referenceTimestamp_upper': 493.0,
-                    'channels': [1],
-                    'referenceTimestamp': 2118068545707,
-                    'file_version': 2,
-                    'magic_start': 322379495,
-                    'magic_end': 322379495}
+    refHeader = {'firstCycleOffsetBytes': 136.0,
+                 'lineHeaderSizeBytes': 40.0,
+                 'laserPathIdx': 0.0,
+                 'bytesPerCycle': 31270.0,
+                 'linesPerCycle': 302.0,
+                 'superPixelsPerCycle': 9595.0,
+                 'dmdPixelsPerRow': 1280.0,
+                 'dmdPixelsPerColumn': 800.0,
+                 'numChannels': 1.0,
+                 'channelMask': 1.0,
+                 'numSlices': 28.0,
+                 'channelsInterleave': 0.0,
+                 'fpgaSystemClock_Hz': 200000000.0,
+                 'referenceTimestamp_lower': 4015357170.0,
+                 'referenceTimestamp_upper': 187.0,
+                 'channels': [0],
+                 'referenceTimestamp': 807174241522,
+                 'file_version': 2,
+                 'magic_start': 322379495,
+                 'magic_end': 322379495}
 
 
 
@@ -30,16 +30,15 @@ def test_datafile():
     assert hDataFile.header == refHeader
 
     # Test if meta data is loaded correctly
-    assert hDataFile.metaData.acqDuration_s == 300
-    assert hDataFile.metaData.acqDurationCycles == 519167.0
-    assert hDataFile.metaData.acqStartTimeReference == 739194.712170625
+    assert hDataFile.metaData.acqDuration_s == 10
+    assert hDataFile.metaData.acqDurationCycles == 344.0
     assert hDataFile.metaData.aomActive == 0
-    assert hDataFile.metaData.aomVoltage == 5.0
-    assert hDataFile.metaData.remoteFocusPosition_um == 225.0
+    assert hDataFile.metaData.aomVoltage == 1.9500000000000002
+    assert hDataFile.metaData.remoteFocusPosition_um == 226.75
 
     # Check ROIs
-    assert len(hDataFile.metaData.AcquisitionContainer.ROIs) == 3 
-    assert hDataFile.metaData.AcquisitionContainer.ROIs[0].targetRate == 2000
+    assert len(hDataFile.metaData.AcquisitionContainer.ROIs) == 643
+    assert hDataFile.metaData.AcquisitionContainer.ROIs[0].targetRate == 5000
 
 
 
